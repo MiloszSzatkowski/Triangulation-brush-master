784cf58d70280d564f5d84857c027f157512244b
MiloszSzatkowski <milosz.dawid.szatkowski@gmail.com>
2018-05-22 02:45:33 +0200

    added event listener and saving points to file

--- Main.jsx
+++ Main.jsx
@@ -14,38 +14,33 @@ $.localize = true;
 app.notifiers.removeAll();
 
 app.notifiersEnabled = true;
+
+//trigger only on sampler
 app.notifiers.add( "Mk  ", new File((new File($.fileName)).parent + "/Triangulate.jsx"));
 
+var aDoc = app.activeDocument;
+aDoc.flatten();
+aDoc.activeLayer.name = "Color_Base";
 
-// var idslct = charIDToTypeID( "slct" );
-//     var desc5 = new ActionDescriptor();
-//     var idnull = charIDToTypeID( "null" );
-//         var ref1 = new ActionReference();
-//         var idcolorSamplerTool = stringIDToTypeID( "colorSamplerTool" );
-//         ref1.putClass( idcolorSamplerTool );
-//     desc5.putReference( idnull, ref1 );
-//     var iddontRecord = stringIDToTypeID( "dontRecord" );
-//     desc5.putBoolean( iddontRecord, true );
-//     var idforceNotify = stringIDToTypeID( "forceNotify" );
-//     desc5.putBoolean( idforceNotify, true );
-// executeAction( idslct, desc5, DialogModes.NO );
+//rename
+var tr = aDoc.artLayers.add();
+tr.name = "Triangulation";
 
 
-// var idMk = charIDToTypeID( "Mk  " );
-//     var desc101 = new ActionDescriptor();
-//     var idnull = charIDToTypeID( "null" );
-//         var ref68 = new ActionReference();
-//         var idClSm = charIDToTypeID( "ClSm" );
-//         ref68.putClass( idClSm );
-//     desc101.putReference( idnull, ref68 );
-//     var idPstn = charIDToTypeID( "Pstn" );
-//         var desc102 = new ActionDescriptor();
-//         var idHrzn = charIDToTypeID( "Hrzn" );
-//         var idPxl = charIDToTypeID( "#Pxl" );
-//         desc102.putUnitDouble( idHrzn, idPxl, 892.500000 );
-//         var idVrtc = charIDToTypeID( "Vrtc" );
-//         var idPxl = charIDToTypeID( "#Pxl" );
-//         desc102.putUnitDouble( idVrtc, idPxl, 217.500000 );
-//     var idPnt = charIDToTypeID( "Pnt " );
-//     desc101.putObject( idPstn, idPnt, desc102 );
-// executeAction( idMk, desc101, DialogModes.NO );
+//select Sampler
+var idslct = charIDToTypeID( "slct" );
+    var desc5 = new ActionDescriptor();
+    var idnull = charIDToTypeID( "null" );
+        var ref1 = new ActionReference();
+        var idcolorSamplerTool = stringIDToTypeID( "colorSamplerTool" );
+        ref1.putClass( idcolorSamplerTool );
+    desc5.putReference( idnull, ref1 );
+    var iddontRecord = stringIDToTypeID( "dontRecord" );
+    desc5.putBoolean( iddontRecord, true );
+    var idforceNotify = stringIDToTypeID( "forceNotify" );
+    desc5.putBoolean( idforceNotify, true );
+executeAction( idslct, desc5, DialogModes.NO );
+
+var logFile = new File((new File($.fileName)).parent + "/Array.txt");
+logFile.open("w");
+logFile.writeln(" ");
--- Triangulate.jsx
+++ Triangulate.jsx
@@ -2,27 +2,169 @@
 
 preferences.rulerUnits = Units.PIXELS;
 
-// var NewSelection;
+var logFile = new File((new File($.fileName)).parent + "/Array.txt");
+// logFile.open("a");
+// logFile.writeln(",");
+
+var arrP;
+
+if (true) {
+writeData(
+  Math.round(app.activeDocument.colorSamplers[0].position[0].value)+
+    ',' +
+  Math.round(app.activeDocument.colorSamplers[0].position[1].value)+
+    ','
+  );
+          // writeData();
+          // alert("ww");
+          remove_All_Points();
+
+            var b = logFile;
+            b.open('r');
+            points = "";
+            while(!b.eof)
+            points += b.readln();
+            b.close();
+
+            arrP = points.split(",");
+
+          // alert(arrP.toString());
+
+          if (arrP.length>6) {
+            _select(arrP[0],
+                    arrP[1],
+                    arrP[2],
+                    arrP[3],
+                    arrP[4],
+                    arrP[5]
+                  );
+                  applyBlur();
+                  logFile.open("w");
+                  logFile.writeln(" ");
+                  //merge new layer
+                  var idMrgtwo = charIDToTypeID( "Mrg2" );
+                    var desc541 = new ActionDescriptor();
+                executeAction( idMrgtwo, desc541, DialogModes.NO );
+                }
+          }
+          // _select()
+
+
+
+
 //
-// var h = app.activeDocument.width.value;
-// var v = app.activeDocument.width.value;
+// var _file = new File((new File($.fileName)).parent + "/Array.txt");
+// var points = "";
 
-// alert("working");
+// var old_data = "";
+function writeData(new_data) {
+  // old_data = readData();
+  logFile.open("a");
+  logFile.writeln(new_data);
+}
 
-// var idslct = charIDToTypeID( "slct" );
-//     var desc5 = new ActionDescriptor();
+
+function remove_All_Points() {
+    //remove all sample points
+    var idDlt = charIDToTypeID( "Dlt " );
+        var desc37 = new ActionDescriptor();
+        var idnull = charIDToTypeID( "null" );
+            var ref26 = new ActionReference();
+            var idClSm = charIDToTypeID( "ClSm" );
+            var idOrdn = charIDToTypeID( "Ordn" );
+            var idAl = charIDToTypeID( "Al  " );
+            ref26.putEnumerated( idClSm, idOrdn, idAl );
+        desc37.putReference( idnull, ref26 );
+    executeAction( idDlt, desc37, DialogModes.NO );
+}
+
+// function readData() {
+//   var b = _file;
+//   b.open('r');
+//   points = "";
+//   while(!b.eof)
+//   points += b.readln();
+//   b.close();
+//   return points;
+// }
+
+
+
+// var arrPoints = points.split(",");
+// alert(arrPoints);
+
+function applyBlur() {
+  //select
+  var idslct = charIDToTypeID( "slct" );
+      var desc503 = new ActionDescriptor();
+      var idnull = charIDToTypeID( "null" );
+          var ref278 = new ActionReference();
+          var idLyr = charIDToTypeID( "Lyr " );
+          ref278.putName( idLyr, "Color_Base" );
+      desc503.putReference( idnull, ref278 );
+      var idMkVs = charIDToTypeID( "MkVs" );
+      desc503.putBoolean( idMkVs, false );
+      var idLyrI = charIDToTypeID( "LyrI" );
+          var list12 = new ActionList();
+          list12.putInteger( 64 );
+      desc503.putList( idLyrI, list12 );
+  executeAction( idslct, desc503, DialogModes.NO );
+
+//copy content of selection to new layer
+var idCpTL = charIDToTypeID( "CpTL" );
+executeAction( idCpTL, undefined, DialogModes.NO );
+_select(arrP[0],
+        arrP[1],
+        arrP[2],
+        arrP[3],
+        arrP[4],
+        arrP[5]
+      );
+//blur avarage layer
+var idAvrg = charIDToTypeID( "Avrg" );
+executeAction( idAvrg, undefined, DialogModes.NO );
+//deselect
+var idsetd = charIDToTypeID( "setd" );
+    var desc846 = new ActionDescriptor();
+    var idnull = charIDToTypeID( "null" );
+        var ref450 = new ActionReference();
+        var idChnl = charIDToTypeID( "Chnl" );
+        var idfsel = charIDToTypeID( "fsel" );
+        ref450.putProperty( idChnl, idfsel );
+    desc846.putReference( idnull, ref450 );
+    var idT = charIDToTypeID( "T   " );
+    var idOrdn = charIDToTypeID( "Ordn" );
+    var idNone = charIDToTypeID( "None" );
+    desc846.putEnumerated( idT, idOrdn, idNone );
+executeAction( idsetd, desc846, DialogModes.NO );
+}
+
+//
+// if ( app.activeDocument.colorSamplers.length===3) {
+// _select
+// } else if (app.activeDocument.colorSamplers.length>3) {
+//
+// //deselect
+// var idsetd = charIDToTypeID( "setd" );
+//     var desc188 = new ActionDescriptor();
 //     var idnull = charIDToTypeID( "null" );
-//         var ref1 = new ActionReference();
-//         var idcolorSamplerTool = stringIDToTypeID( "colorSamplerTool" );
-//         ref1.putClass( idcolorSamplerTool );
-//     desc5.putReference( idnull, ref1 );
-//     var iddontRecord = stringIDToTypeID( "dontRecord" );
-//     desc5.putBoolean( iddontRecord, true );
-//     var idforceNotify = stringIDToTypeID( "forceNotify" );
-//     desc5.putBoolean( idforceNotify, true );
-// executeAction( idslct, desc5, DialogModes.NO );
+//         var ref120 = new ActionReference();
+//         var idChnl = charIDToTypeID( "Chnl" );
+//         var idfsel = charIDToTypeID( "fsel" );
+//         ref120.putProperty( idChnl, idfsel );
+//     desc188.putReference( idnull, ref120 );
+//     var idT = charIDToTypeID( "T   " );
+//     var idOrdn = charIDToTypeID( "Ordn" );
+//     var idNone = charIDToTypeID( "None" );
+//     desc188.putEnumerated( idT, idOrdn, idNone );
+// executeAction( idsetd, desc188, DialogModes.NO );
+//
 
-if ( app.activeDocument.colorSamplers.length===3) {
+// }
+//
+//
+//
+function _select(a1,a2,b1,b2,c1,c2) {
   var idsetd = charIDToTypeID( "setd" );
       var desc54 = new ActionDescriptor();
       var idnull = charIDToTypeID( "null" );
@@ -38,37 +180,37 @@ if ( app.activeDocument.colorSamplers.length===3) {
                   var desc56 = new ActionDescriptor();
                   var idHrzn = charIDToTypeID( "Hrzn" );
                   var idPxl = charIDToTypeID( "#Pxl" );
-                  desc56.putUnitDouble( idHrzn, idPxl, app.activeDocument.colorSamplers[0].position[0].value );
+                  desc56.putUnitDouble( idHrzn, idPxl, a1);
                   var idVrtc = charIDToTypeID( "Vrtc" );
                   var idPxl = charIDToTypeID( "#Pxl" );
-                  desc56.putUnitDouble( idVrtc, idPxl, app.activeDocument.colorSamplers[0].position[1].value);
+                  desc56.putUnitDouble( idVrtc, idPxl, a2);
               var idPnt = charIDToTypeID( "Pnt " );
               list1.putObject( idPnt, desc56 );
                   var desc57 = new ActionDescriptor();
                   var idHrzn = charIDToTypeID( "Hrzn" );
                   var idPxl = charIDToTypeID( "#Pxl" );
-                  desc57.putUnitDouble( idHrzn, idPxl, app.activeDocument.colorSamplers[1].position[0].value  );
+                  desc57.putUnitDouble( idHrzn, idPxl, b1 );
                   var idVrtc = charIDToTypeID( "Vrtc" );
                   var idPxl = charIDToTypeID( "#Pxl" );
-                  desc57.putUnitDouble( idVrtc, idPxl, app.activeDocument.colorSamplers[1].position[1].value );
+                  desc57.putUnitDouble( idVrtc, idPxl, b2 );
               var idPnt = charIDToTypeID( "Pnt " );
               list1.putObject( idPnt, desc57 );
                   var desc58 = new ActionDescriptor();
                   var idHrzn = charIDToTypeID( "Hrzn" );
                   var idPxl = charIDToTypeID( "#Pxl" );
-                  desc58.putUnitDouble( idHrzn, idPxl, app.activeDocument.colorSamplers[2].position[0].value);
+                  desc58.putUnitDouble( idHrzn, idPxl, c1);
                   var idVrtc = charIDToTypeID( "Vrtc" );
                   var idPxl = charIDToTypeID( "#Pxl" );
-                  desc58.putUnitDouble( idVrtc, idPxl, app.activeDocument.colorSamplers[2].position[1].value );
+                  desc58.putUnitDouble( idVrtc, idPxl, c2);
               var idPnt = charIDToTypeID( "Pnt " );
               list1.putObject( idPnt, desc58 );
                   var desc59 = new ActionDescriptor();
                   var idHrzn = charIDToTypeID( "Hrzn" );
                   var idPxl = charIDToTypeID( "#Pxl" );
-                  desc59.putUnitDouble( idHrzn, idPxl, app.activeDocument.colorSamplers[0].position[0].value );
+                  desc59.putUnitDouble( idHrzn, idPxl, a1 );
                   var idVrtc = charIDToTypeID( "Vrtc" );
                   var idPxl = charIDToTypeID( "#Pxl" );
-                  desc59.putUnitDouble( idVrtc, idPxl, app.activeDocument.colorSamplers[0].position[1].value );
+                  desc59.putUnitDouble( idVrtc, idPxl, a2);
               var idPnt = charIDToTypeID( "Pnt " );
               list1.putObject( idPnt, desc59 );
           desc55.putList( idPts, list1 );
@@ -77,52 +219,4 @@ if ( app.activeDocument.colorSamplers.length===3) {
       var idAntA = charIDToTypeID( "AntA" );
       desc54.putBoolean( idAntA, true );
   executeAction( idsetd, desc54, DialogModes.NO );
-} else if (app.activeDocument.colorSamplers.length>3) {
-
-//deselect
-var idsetd = charIDToTypeID( "setd" );
-    var desc188 = new ActionDescriptor();
-    var idnull = charIDToTypeID( "null" );
-        var ref120 = new ActionReference();
-        var idChnl = charIDToTypeID( "Chnl" );
-        var idfsel = charIDToTypeID( "fsel" );
-        ref120.putProperty( idChnl, idfsel );
-    desc188.putReference( idnull, ref120 );
-    var idT = charIDToTypeID( "T   " );
-    var idOrdn = charIDToTypeID( "Ordn" );
-    var idNone = charIDToTypeID( "None" );
-    desc188.putEnumerated( idT, idOrdn, idNone );
-executeAction( idsetd, desc188, DialogModes.NO );
-
-  //remove all sample points
-  var idDlt = charIDToTypeID( "Dlt " );
-      var desc37 = new ActionDescriptor();
-      var idnull = charIDToTypeID( "null" );
-          var ref26 = new ActionReference();
-          var idClSm = charIDToTypeID( "ClSm" );
-          var idOrdn = charIDToTypeID( "Ordn" );
-          var idAl = charIDToTypeID( "Al  " );
-          ref26.putEnumerated( idClSm, idOrdn, idAl );
-      desc37.putReference( idnull, ref26 );
-  executeAction( idDlt, desc37, DialogModes.NO );
 }
-
-
-
-
-
-
-
-
-
-
-// alert("x = " + pos[0] + " y = " + pos[1]);
-//
-// NewSelection = Array (Array(0,0),
-//                       Array( w,0),
-//                       Array( w,h),
-//                       Array(0,0));
-//
-// app.activeDocument.selection.select(NewSelection);
-//
-// app.activeDocument.artLayers.add();
